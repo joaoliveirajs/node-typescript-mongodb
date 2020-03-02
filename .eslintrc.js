f423ccafb4f2a9170de0e9b8c76f3986057e51a9
joaoliveirajs <j.pschulzspf@gmail.com>
2020-03-01 22:08:34 -0300

    Chore: ESlint and Prettier for linting

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
+    }
+};
