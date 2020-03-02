1c6c1fe025c566162b627c90cfc23f43cf531b04
joaoliveirajs <j.pschulzspf@gmail.com>
2020-03-01 21:58:33 -0300

    Chore: added routes

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+import UserController from './controllers/UserController';
+
+const routes = Router();
+
+routes.delete('/users/:id', UserController.destroy);
+routes.get('/users/:id', UserController.show);
+routes.get('/users', UserController.index);
+routes.patch('/users', UserController.update);
+routes.post('/users', UserController.store);
+
+export default routes;
