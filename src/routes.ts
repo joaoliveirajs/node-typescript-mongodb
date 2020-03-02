810df74f37f9a50c951aa0d866f1226fbe058282
joaoliveirajs <j.pschulzspf@gmail.com>
2020-03-01 23:36:27 -0300

    Feat: added redirect to /users in root route

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import UserController from './controllers/UserController';
 
 const routes = Router();
 
+routes.get('/', (req, res) => res.redirect('/users'));
+
 routes.delete('/users/:id', UserController.destroy);
 routes.get('/users/:id', UserController.show);
 routes.get('/users', UserController.index);
